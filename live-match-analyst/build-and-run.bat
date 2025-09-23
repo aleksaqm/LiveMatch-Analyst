f86c5ff42d61001a17c517063221b70cfeb2057e
cornenkiV <vladimircornenki02@gmail.com>
2025-09-23 21:57:11 +0200

    feat: CEP 2

--- live-match-analyst/build-and-run.bat
+++ live-match-analyst/build-and-run.bat
@@ -1,17 +1,20 @@
 @echo off
 cd /d "%~dp0"
 
+set JAVA_HOME=C:\Users\Vladimir\.jdks\corretto-11.0.28
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 echo Building model
 cd model
-call mvn clean install -B
+call .\mvnw.cmd clean install -B
 cd ..
 
 echo Building kjar
 cd kjar
-call mvn clean install -B
+call .\mvnw.cmd clean install -B
 if errorlevel 1 exit /b %errorlevel%
 cd ..
 
 echo Starting Spring Boot service...
 cd service
-mvn spring-boot:run
+.\mvnw.cmd spring-boot:run
